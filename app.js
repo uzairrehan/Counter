65be6656a348882bf9e7f6c42d69347d98ad45cf
Syed Uzair Shah <iamsyeduzairshah@gmail.com>
2024-05-09 20:35:14 +0500

    Counter created all ok

--- app.js
+++ app.js
@@ -5,25 +5,22 @@ let error = document.querySelectorAll("#error");
 
 
 function addElement() {
-let displayValue = display[0].value;   
-if (!isNaN(displayValue)){
-    number[0].innerHTML = Number(number[0].innerHTML) + Number(displayValue);
-    error[0].innerHTML = "" ;
+    let displayValue = display[0].value;
+    if (!isNaN(displayValue)) {
+        number[0].innerHTML = Number(number[0].innerHTML) + Number(displayValue);
+        error[0].innerHTML = "";
+    }
+    else {
+        error[0].innerHTML = "please enter a number";
+    }
 }
-else{
-    error[0].innerHTML = "please enter a number";
-}
-}
-
 function minusElement() {
-    let displayValue = display[0].value;   
-    number[0].innerHTML = Number(number[0].innerHTML) - Number(displayValue);   
+    let displayValue = display[0].value;
+    number[0].innerHTML = Number(number[0].innerHTML) - Number(displayValue);
+
 }
-
-
-
-function clear() {
-let displayValue = display[0].value;   
-    displayValue = "1";
-    number[0].innerHTML = 0;
+function clearAll() {
+    error[0].innerHTML = "";
+    display[0].value = "";
+    number[0].innerHTML = "";
 }
